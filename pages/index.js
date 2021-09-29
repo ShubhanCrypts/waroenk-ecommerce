58d9f05429cc853ad5bd12c60c4162f6f841da2f
ShubhanCrypts <Shubhan1901@gmail.com>
2021-09-29 21:45:44 +0700

    revision products css, done

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,29 @@
 import { getData } from "../utils/fetchData";
 import { useState } from "react";
-import Head from 'next/head'
+import Head from "next/head";
 import ProductItem from "../components/product/ProductItem";
 
 const Home = (props) => {
   const [products, setProducts] = useState(props.products);
 
-  return(
-    <div className= "product">
+  return (
+    <div className="products">
       <Head>
         <title>Home Page</title>
       </Head>
-      {
-        products.length === 0 
-        ? <h2>No product</h2>
-
-        : products.map(product => (
+      {products.length === 0 ? (
+        <h2>No product</h2>
+      ) : (
+        products.map((product) => (
           <ProductItem key={product._id} product={product} />
         ))
-      }
+      )}
     </div>
-
   );
 };
 
 export async function getServerSideProps() {
-  const res = await getData('product');
+  const res = await getData("product");
 
   return {
     props: {
