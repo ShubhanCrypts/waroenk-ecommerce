bda8e3020ffc7c72878df6e293b21ebee097658e
ShubhanCrypts <Shubhan1901@gmail.com>
2021-09-29 19:32:58 +0700

    half done

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   reactStrictMode: true,
-}
+  env: {
+    BASE_URL: "http://localhost:3000",
+    MONGODB_URL:
+      "mongodb://waroenk_admin:waroenk123@cluster0-shard-00-00.kbuvq.mongodb.net:27017,cluster0-shard-00-01.kbuvq.mongodb.net:27017,cluster0-shard-00-02.kbuvq.mongodb.net:27017/waroenk_ecommerce?ssl=true&replicaSet=atlas-flct0f-shard-0&authSource=admin&retryWrites=true&w=majority",
+  },
+};
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
-const Home = () => {
+import { getData } from "../utils/fetchData";
+import { useState } from "react";
+
+const Home = (props) => {
+  const [products, setProducts] = useState(props.products);
+
+  console.log(products);
+
   return <div>Home</div>;
 };
+
+export async function getServerSideProps() {
+  const res = await getData("product");
+  console.log(res);
+
+  return {
+    props: {
+      products: res.products,
+      result: res.result,
+    },
+  };
+}
 
 export default Home;
