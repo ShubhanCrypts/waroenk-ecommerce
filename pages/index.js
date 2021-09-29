303ee82b38ed7f4f942eec82a054d8bd3c208664
Muhammad Ikhlas <mada.ikhlas@gmail.com>
2021-09-29 21:16:18 +0700

    Success on showing product

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,31 @@
 import { getData } from "../utils/fetchData";
 import { useState } from "react";
+import Head from 'next/head'
+import ProductItem from "../components/product/ProductItem";
 
 const Home = (props) => {
   const [products, setProducts] = useState(props.products);
 
-  console.log(products);
+  return(
+    <div className= "product">
+      <Head>
+        <title>Home Page</title>
+      </Head>
+      {
+        products.length === 0 
+        ? <h2>No product</h2>
 
-  return <div>Home</div>;
+        : products.map(product => (
+          <ProductItem key={product._id} product={product} />
+        ))
+      }
+    </div>
+
+  );
 };
 
 export async function getServerSideProps() {
-  const res = await getData("product");
-  console.log(res);
+  const res = await getData('product');
 
   return {
     props: {
