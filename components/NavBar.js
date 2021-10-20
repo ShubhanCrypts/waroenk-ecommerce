12ae01ee075660b60582cf08934d354d82563c73
Muhammad Ikhlas <mada.ikhlas@gmail.com>
2021-10-20 19:33:03 +0700

    msh gagal

--- components/NavBar.js
+++ components/NavBar.js
@@ -23,6 +23,22 @@ function NavBar() {
     // Object.keys(auth).length = 0;
   };
 
+  const adminRouter = () => {
+    return(
+        <>
+        <Link href="/users">
+            <a className="dropdown-item">Users</a>
+        </Link>
+        <Link href="/create">
+            <a className="dropdown-item">Products</a>
+        </Link>
+        <Link href="/categories">
+            <a className="dropdown-item">Categories</a>
+        </Link>
+        </>
+    )
+  };
+
   const loggedRouter = () => {
     return (
       <li className="nav-item dropdown">
@@ -49,10 +65,13 @@ function NavBar() {
           {auth.user.name}
         </a>
         <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a className="dropdown-item" href="#">
-            Profile
-          </a>
-
+          <Link href="/profile">
+            <a className="dropdown-item">Profile</a>
+          </Link>
+          {
+            auth.user.role === 'admin' && adminRouter()
+          }
+          <div className="dropdown-divider"></div>
           <button className="dropdown-item" href="#" onClick={handleLogout}>
             Logout
           </button>
--- pages/api/product/index.js
+++ pages/api/product/index.js
@@ -1,12 +1,16 @@
 import connectDB from "../../../utils/connectDB";
 import Products from "../../../models/productModel";
+import auth from '../../../middleware/auth'
 
 connectDB();
 
 export default async (req, res) => {
   switch (req.method) {
     case "GET":
-      await getProducts(req, res);
+      await getProducts(req, res)
+      break;
+    case "POST":
+      await createProduct(req, res)
       break;
   }
 };
@@ -21,5 +25,31 @@ const getProducts = async (req, res) => {
     });
   } catch (err) {
     return res.status(500).json({ err: err.message });
+  }
+};
+
+const createProduct = async (req, res) => {
+  try {
+    const result = await auth(req, res)
+    if(result.role !== 'admin') return res.status(400).json({err: 'Authentication is not valid.'})
+
+    const {title, price, inStock, description, content, category, images} = req.body
+
+    if(!title || !price || !inStock || !description || !content || category === 'all' || images.length === 0)
+    return res.status(400).json({err: 'Please add all the fields.'})
+
+    const product = await Products.findOne({product_id})
+    if(product) return res.status(400).json({err: 'This product already exist.'})
+
+    const newProduct = new Products({
+      title: title.toLowerCase(), price, inStock, description, content, category, images
+    })
+
+    await newProduct.save()
+
+    res.json({msg: 'Success! Created a new product'})
+
+  }catch (err) {
+    return res.status(500).json({err: err.message})
   }
 };
