bfafd6ca61b52f38ae5def3afbb357532b9ecd87
ShubhanCrypts <Shubhan1901@gmail.com>
2021-11-02 18:49:29 +0700

    not working yet

--- components/product/ProductItem.js
+++ components/product/ProductItem.js
@@ -1,80 +1,112 @@
-import Link from 'next/link'
-import { useContext } from 'react'
-import { DataContext } from '../../store/GlobalState'
-import { addToCart } from '../../store/Actions'
+import Link from 'next/link';
+import { useContext } from 'react';
+import { DataContext } from '../../store/GlobalState';
+import { addToCart } from '../../store/Actions';
 
-const ProductItem = ({product}) => {
-    const { state, dispatch } = useContext(DataContext)
-    const { cart, auth } = state
+const ProductItem = ({ product }) => {
+  const { state, dispatch } = useContext(DataContext);
+  const { cart, auth } = state;
+  const productViewData = product;
 
-    const userLink = () => {
-        return(
-            <>
-                <Link href={`product/${product._id}`}>
-                    <a className="btn btn-info"
-                    style={{marginRight: '5px', flex: 1}}>View</a>
-                </Link>
-                <button className="btn btn-success"
-                style={{marginLeft: '5px', flex: 1}}
-                disabled={product.inStock === 0 ? true : false} 
-                onClick={() => dispatch(addToCart(product, cart))} >
-                    Buy
-                </button>
-            </>
-        )
-    }
+  const userLink = () => {
+    return (
+      <>
+        <Link
+          onClick={handleSubmit}
+          type="submit"
+          href={`product/${product._id}`}
+        >
+          <a className="btn btn-info" style={{ marginRight: '5px', flex: 1 }}>
+            View
+          </a>
+        </Link>
+        <button
+          className="btn btn-success"
+          style={{ marginLeft: '5px', flex: 1 }}
+          disabled={product.inStock === 0 ? true : false}
+          onClick={() => dispatch(addToCart(product, cart))}
+        >
+          Buy
+        </button>
+      </>
+    );
+  };
 
-    const adminLink = () => {
-        return(
-            <>
-                <Link href={`create/${product._id}`}>
-                    <a className="btn btn-info"
-                    style={{marginRight: '5px', flex: 1}}>Edit</a>
-                </Link>
-                <button className="btn btn-danger"
-                style={{marginLeft: '5px', flex: 1}}
-                data-toggle="modal" data-target="#exampleModal"
-                onClick={() => dispatch({
-                    type: 'ADD_MODAL',
-                    payload: [{ 
-                        data: '', id: product._id, 
-                        title: product.title, type: 'DELETE_PRODUCT' 
-                    }]
-                })} >
-                    Delete
-                </button>
-            </>
-        )
-    }
-    
-    return(
-        <div className="card" style={{ width: '18rem' }}>
-            <img className="card-img-top" src={product.images[0].url} alt={product.images[0].url} />
-            <div className="card-body">
-                <h5 className="card-title text-capitalize" title={product.title}>
-                    {product.title}
-                </h5>
+  const adminLink = () => {
+    return (
+      <>
+        <Link href={`create/${product._id}`}>
+          <a className="btn btn-info" style={{ marginRight: '5px', flex: 1 }}>
+            Edit
+          </a>
+        </Link>
+        <button
+          className="btn btn-danger"
+          style={{ marginLeft: '5px', flex: 1 }}
+          data-toggle="modal"
+          data-target="#exampleModal"
+          onClick={() =>
+            dispatch({
+              type: 'ADD_MODAL',
+              payload: [
+                {
+                  data: '',
+                  id: product._id,
+                  title: product.title,
+                  type: 'DELETE_PRODUCT',
+                },
+              ],
+            })
+          }
+        >
+          Delete
+        </button>
+      </>
+    );
+  };
 
-                <div className="row justify-content-between mx-0">
-                    <h6 className="text-danger">Rp{product.price}</h6>
-                    {
-                        product.inStock > 0
-                        ? <h6 className="text-danger">In Stock: {product.inStock}</h6>
-                        : <h6 className="text-danger">Out Stock</h6>
-                    }
-                </div>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-                <p className="card-text" title={product.description}>
-                    {product.description}
-                </p>
+    const res = await postData('product', productViewData);
 
-                <div className="row justify-content-between mx-0">
-                    {!auth.user || auth.user.role !== "admin" ? userLink() : adminLink()}
-                </div>
-                    
-            </div>
+    if (res.err)
+      return dispatch({ type: 'NOTIFY', payload: { error: res.err } });
+
+    return dispatch({ type: 'NOTIFY', payload: { success: res.msg } });
+  };
+
+  return (
+    <div className="card" style={{ width: '18rem' }}>
+      <img
+        className="card-img-top"
+        src={product.images[0].url}
+        alt={product.images[0].url}
+      />
+      <div className="card-body">
+        <h5 className="card-title text-capitalize" title={product.title}>
+          {product.title}
+        </h5>
+
+        <div className="row justify-content-between mx-0">
+          <h6 className="text-danger">Rp{product.price}</h6>
+          {product.inStock > 0 ? (
+            <h6 className="text-danger">In Stock: {product.inStock}</h6>
+          ) : (
+            <h6 className="text-danger">Out Stock</h6>
+          )}
         </div>
-    )
-}
 
-export default ProductItem
+        <p className="card-text" title={product.description}>
+          {product.description}
+        </p>
+
+        <div className="row justify-content-between mx-0">
+          {!auth.user || auth.user.role !== 'admin' ? userLink() : adminLink()}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductItem;
--- pages/api/product/[id].js
+++ pages/api/product/[id].js
@@ -1,19 +1,23 @@
-import connectDB from "../../../utils/connectDB";
-import Products from "../../../models/productModel";
-import auth from '../../../middleware/auth'
+import connectDB from '../../../utils/connectDB';
+import Products from '../../../models/productModel';
+import auth from '../../../middleware/auth';
+import ProductView from '../../../models/productViewTrackerModel';
 
 connectDB();
 
 export default async (req, res) => {
   switch (req.method) {
-    case "GET":
-      await getProduct(req, res)
+    case 'GET':
+      await getProduct(req, res);
       break;
-    case "PUT":
-      await updateProduct(req, res)
+    case 'PUT':
+      await updateProduct(req, res);
       break;
-    case "DELETE":
-      await deleteProduct(req, res)
+    case 'DELETE':
+      await deleteProduct(req, res);
+      break;
+    case 'POST':
+      await postViewProduct(req, res);
       break;
   }
 };
@@ -23,7 +27,7 @@ const getProduct = async (req, res) => {
     const { id } = req.query;
     const product = await Products.findById(id);
     if (!product)
-      return res.status(400).json({ err: "Produk tidak ditemukan" });
+      return res.status(400).json({ err: 'Produk tidak ditemukan' });
 
     res.json({ product });
   } catch (err) {
@@ -33,39 +37,95 @@ const getProduct = async (req, res) => {
 
 const updateProduct = async (req, res) => {
   try {
-    const result = await auth(req, res)
-    if(result.role !== 'admin') return res.status(400).json({err: 'Authentication is not valid.'})
+    const result = await auth(req, res);
+    if (result.role !== 'admin')
+      return res.status(400).json({ err: 'Authentication is not valid.' });
 
-    const {id} = req.query
-    const {title, price, inStock, description, content, category, images} = req.body
+    const { id } = req.query;
+    const { title, price, inStock, description, content, category, images } =
+      req.body;
 
-    if(!title || !price || !inStock || !description || !content || category === 'all' || images.length === 0)
-    return res.status(400).json({err: 'Please add all the fields.'})
+    if (
+      !title ||
+      !price ||
+      !inStock ||
+      !description ||
+      !content ||
+      category === 'all' ||
+      images.length === 0
+    )
+      return res.status(400).json({ err: 'Please add all the fields.' });
 
-    await Products.findOneAndUpdate({_id: id}, {
-      title: title.toLowerCase(), price, inStock, description, content, category, images
-    })
+    await Products.findOneAndUpdate(
+      { _id: id },
+      {
+        title: title.toLowerCase(),
+        price,
+        inStock,
+        description,
+        content,
+        category,
+        images,
+      }
+    );
 
-    res.json({msg: 'Success! Updated a product'})
-  }catch (err) {
-    return res.status(500).json({err: err.message})
+    res.json({ msg: 'Success! Updated a product' });
+  } catch (err) {
+    return res.status(500).json({ err: err.message });
   }
 };
 
-const deleteProduct = async(req, res) => {
+const deleteProduct = async (req, res) => {
   try {
-    const result = await auth(req, res)
-      
-    if(result.role !== 'admin') 
-    return res.status(400).json({err: 'Authentication is not valid.'})
+    const result = await auth(req, res);
 
-    const {id} = req.query
+    if (result.role !== 'admin')
+      return res.status(400).json({ err: 'Authentication is not valid.' });
 
-    await Products.findByIdAndDelete(id)
-    res.json({msg: 'Deleted a product.'})
+    const { id } = req.query;
 
-  }catch (err) {
-    return res.status(500).json({err: err.message})
+    await Products.findByIdAndDelete(id);
+    res.json({ msg: 'Deleted a product.' });
+  } catch (err) {
+    return res.status(500).json({ err: err.message });
   }
 };
 
+const postViewProduct = async (req, res) => {
+  try {
+    const {
+      title,
+      price,
+      description,
+      content,
+      images,
+      category,
+      checked,
+      inStock,
+      sold,
+    } = req.body;
+
+    // const productView = await Users.findOne({ email });
+    // if (user)
+    //   return res.status(400).json({ err: 'This email already exists.' });
+
+    // const passwordHash = await bcrypt.hash(password, 12);
+
+    const newProductView = new ProductView({
+      title,
+      price,
+      description,
+      content,
+      images,
+      category,
+      checked,
+      inStock,
+      sold,
+    });
+
+    await newProductView.save();
+    res.json({ msg: 'Register Success!' });
+  } catch (err) {
+    return res.status(500).json({ err: err.message });
+  }
+};
