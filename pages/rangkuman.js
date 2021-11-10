5dfb9011798316524af52763cdf4883c9a648952
alimahfud01 <alimahfud2001@gmail.com>
2021-11-10 21:51:50 +0700

    Blm ada kurir

--- pages/rangkuman.js
+++ pages/rangkuman.js
@@ -1,12 +1,15 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import CartItem from '../components/CartItem';
-
+import { useContext, useState, useEffect } from 'react';
+import Cartlist from './Cartlist';
+import { DataContext } from '../store/GlobalState';
 
 const Rangkuman = () => {
+    const { state, dispatch } = useContext(DataContext);
+    const { cart, auth, orders } = state;
     const router = useRouter();
     const {
-        query: { alamat, jumlah, cart},} = router;
+        query: { alamat, jumlah, payment, kurir},} = router;
 
     return (
     <div className='row mx-auto'>
@@ -22,9 +25,14 @@ const Rangkuman = () => {
                         <td className="p-2">PESANAN</td>
                     </tr>
                     </thead>
-                        <tr>
-                            <td className="p-2">{cart}</td>
-                        </tr>
+                        {cart.map((item) => (
+                        <Cartlist
+                            key={item._id}
+                            item={item}
+                            dispatch={dispatch}
+                            cart={cart}
+                        />
+                        ))}
                     <thead className="bg-light font-weight-bold">
                     <tr>
                         <td className="p-2">ALAMAT</td>
@@ -41,17 +49,18 @@ const Rangkuman = () => {
                     <tr>
                         <td className="p-2">TOTAL</td>
                         <td className="p-2">METODE PEMBAYARAN</td>
+                        <td className="p-2">KURIR</td>
                     </tr>
                     </thead>
                         <tr>
                             <td className="p-2">{jumlah}</td>
-                            <td className="p-2">Metode</td>
+                            <td className="p-2">{payment}</td>
+                            <td className="p-2">{kurir}</td>
                         </tr>
                 </table>
             </div>
             <Link href="/product">
-                <a className='btn btn-dark my-3'>
-                Proses Pembayaran</a>
+                <a className='btn btn-dark my-3'>Pay</a>
                 </Link>         
         </div>
 
