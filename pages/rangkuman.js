5b23d7b13a3d9c7d01059cd2d21d6aa229b6875a
Muhammad Ikhlas <mada.ikhlas@gmail.com>
2021-11-17 18:50:53 +0700

    cart.js+rangkuman.js

--- pages/rangkuman.js
+++ pages/rangkuman.js
@@ -1,67 +1,74 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-import CartItem from "../components/CartItem";
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useContext, useState, useEffect } from 'react';
+import Cartlist from './Cartlist';
+import { DataContext } from '../store/GlobalState';
 
 const Rangkuman = () => {
-  const router = useRouter();
-  const {
-    query: { alamat, jumlah, cart, kurir },
-  } = router;
+    const { state, dispatch } = useContext(DataContext);
+    const { cart, auth, orders } = state;
+    const router = useRouter();
+    const {
+        query: { alamat, jumlah, payment, kurir},} = router;
 
-  return (
-    <div className="row mx-auto">
-      <div className="col-md-8 text-secondary table-responsive my-3">
-        <h2 className="text-uppercase">Order Summary</h2>
+    return (
+    <div className='row mx-auto'>
+        <div className='col-md-8 text-secondary table-responsive my-3'>
+        <h2 className='text-uppercase'>Order Summary</h2>
 
         <div className="col-md-8">
-          <div className="my-3 table-responsive">
-            <table
-              className="table-bordered  w-100"
-              style={{ minWidth: "300px", cursor: "pointer" }}
-            >
-              <thead className="bg-light font-weight-bold">
-                <tr>
-                  <td className="p-2">PESANAN</td>
-                </tr>
-              </thead>
-              <tr>
-                <td className="p-2">{cart}</td>
-              </tr>
-              <thead className="bg-light font-weight-bold">
-                <tr>
-                  <td className="p-2">ALAMAT</td>
-                </tr>
-              </thead>
-              <tr>
-                <td className="p-2">{alamat}</td>
-              </tr>
-            </table>
+            <div className="my-3 table-responsive">
+                <table className="table-bordered  w-100"
+                    style={{minWidth: '300px', cursor: 'pointer'}}>
+                    <thead className="bg-light font-weight-bold">
+                    <tr>
+                        <td className="p-2">PESANAN</td>
+                    </tr>
+                    </thead>
+                        {cart.map((item) => (
+                        <Cartlist
+                            key={item._id}
+                            item={item}
+                            dispatch={dispatch}
+                            cart={cart}
+                        />
+                        ))}
+                    <thead className="bg-light font-weight-bold">
+                    <tr>
+                        <td className="p-2">ALAMAT</td>
+                    </tr>
+                    </thead>
+                        <tr>
+                            <td className="p-2">{alamat}</td>
+                        </tr>
+                </table>
 
-            <table
-              className="table-bordered  w-100"
-              style={{ minWidth: "300px", cursor: "pointer" }}
-            >
-              <thead className="bg-light font-weight-bold">
-                <tr>
-                  <td className="p-2">TOTAL</td>
-                  <td className="p-2">METODE PEMBAYARAN</td>
-                  <td className="p-2">KURIR</td>
-                </tr>
-              </thead>
-              <tr>
-                <td className="p-2">{jumlah}</td>
-                <td className="p-2">Metode</td>
-                <td className="p-2">{kurir}</td>
-              </tr>
-            </table>
-          </div>
-          <Link href="/product">
-            <a className="btn btn-dark my-3">Proses Pembayaran</a>
-          </Link>
+                <table className="table-bordered  w-100"
+                    style={{minWidth: '300px', cursor: 'pointer'}}>
+                    <thead className="bg-light font-weight-bold">
+                    <tr>
+                        <td className="p-2">TOTAL</td>
+                        <td className="p-2">METODE PEMBAYARAN</td>
+                        <td className="p-2">KURIR</td>
+                    </tr>
+                    </thead>
+                        <tr>
+                            <td className="p-2">{jumlah}</td>
+                            <td className="p-2">{payment}</td>
+                            <td className="p-2">{kurir}</td>
+                        </tr>
+                </table>
+            </div>
+            <Link href="/product">
+                <a className='btn btn-dark my-3'>Pay</a>
+                </Link>         
         </div>
-      </div>
-    </div>
-  );
-};
 
-export default Rangkuman;
+      </div>
+      </div>
+    );
+
+  };
+  
+  export default Rangkuman;
+  
