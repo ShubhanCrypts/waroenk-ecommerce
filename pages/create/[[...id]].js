203919a94fdc1f0acff004768afde7510e1f30cb
ShubhanCrypts <Shubhan1901@gmail.com>
2021-11-17 18:20:43 +0700

    upload product

--- pages/create/[[...id]].js
+++ pages/create/[[...id]].js
@@ -1,18 +1,18 @@
-import Head from 'next/head';
-import { useState, useContext, useEffect } from 'react';
-import { DataContext } from '../../store/GlobalState';
-import { imageUpload } from '../../utils/imageUpload';
-import { postData, getData, putData } from '../../utils/fetchData';
-import { useRouter } from 'next/router';
+import Head from "next/head";
+import { useState, useContext, useEffect } from "react";
+import { DataContext } from "../../store/GlobalState";
+import { imageUpload } from "../../utils/imageUpload";
+import { postData, getData, putData } from "../../utils/fetchData";
+import { useRouter } from "next/router";
 
 const ProductsManager = () => {
   const initialState = {
-    title: '',
+    title: "",
     price: 0,
     inStock: 0,
-    description: '',
-    content: '',
-    category: '',
+    description: "",
+    content: "",
+    category: "5faa35b58fdff228384d51da",
   };
   const [product, setProduct] = useState(initialState);
   const { title, price, inStock, description, content, category } = product;
@@ -43,41 +43,41 @@ const ProductsManager = () => {
   const handleChangeInput = (e) => {
     const { name, value } = e.target;
     setProduct({ ...product, [name]: value });
-    dispatch({ type: 'NOTIFY', payload: {} });
+    dispatch({ type: "NOTIFY", payload: {} });
   };
 
   const handleUploadInput = (e) => {
-    dispatch({ type: 'NOTIFY', payload: {} });
+    dispatch({ type: "NOTIFY", payload: {} });
     let newImages = [];
     let num = 0;
-    let err = '';
+    let err = "";
     const files = [...e.target.files];
 
     if (files.length === 0)
       return dispatch({
-        type: 'NOTIFY',
-        payload: { error: 'Files does not exist.' },
+        type: "NOTIFY",
+        payload: { error: "Files does not exist." },
       });
 
     files.forEach((file) => {
       if (file.size > 1024 * 1024)
-        return (err = 'The largest image size is 1mb');
+        return (err = "The largest image size is 1mb");
 
-      if (file.type !== 'image/jpeg' && file.type !== 'image/png')
-        return (err = 'Image format is incorrect.');
+      if (file.type !== "image/jpeg" && file.type !== "image/png")
+        return (err = "Image format is incorrect.");
 
       num += 1;
       if (num <= 5) newImages.push(file);
       return newImages;
     });
 
-    if (err) dispatch({ type: 'NOTIFY', payload: { error: err } });
+    if (err) dispatch({ type: "NOTIFY", payload: { error: err } });
 
     const imgCount = images.length;
     if (imgCount + newImages.length > 5)
       return dispatch({
-        type: 'NOTIFY',
-        payload: { error: 'Select up to 5 images.' },
+        type: "NOTIFY",
+        payload: { error: "Select up to 5 images." },
       });
     setImages([...images, ...newImages]);
   };
@@ -90,11 +90,11 @@ const ProductsManager = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (auth.user.role !== 'admin')
-      return dispatch({
-        type: 'NOTIFY',
-        payload: { error: 'Authentication is not valid.' },
-      });
+    // if (auth.user.role !== "admin")
+    //   return dispatch({
+    //     type: "NOTIFY",
+    //     payload: { error: "Authentication is not valid." },
+    //   });
 
     if (
       !title ||
@@ -102,49 +102,49 @@ const ProductsManager = () => {
       !inStock ||
       !description ||
       !content ||
-      category === 'all' ||
+      category === "all" ||
       images.length === 0
     )
       return dispatch({
-        type: 'NOTIFY',
-        payload: { error: 'Please add all the fields.' },
+        type: "NOTIFY",
+        payload: { error: "Please add all the fields." },
       });
 
-    dispatch({ type: 'NOTIFY', payload: { loading: true } });
+    dispatch({ type: "NOTIFY", payload: { loading: true } });
     let media = [];
     const imgNewURL = images.filter((img) => !img.url);
     const imgOldURL = images.filter((img) => img.url);
 
     if (imgNewURL.length > 0) media = await imageUpload(imgNewURL);
 
-    let res;
-    if (onEdit) {
-      res = await putData(
-        `product/${id}`,
-        { ...product, images: [...imgOldURL, ...media] },
-        auth.token
-      );
-      if (res.err)
-        return dispatch({ type: 'NOTIFY', payload: { error: res.err } });
-    } else {
-      res = await postData(
-        'product',
-        { ...product, images: [...imgOldURL, ...media] },
-        auth.token
-      );
-      if (res.err)
-        return dispatch({ type: 'NOTIFY', payload: { error: res.err } });
-    }
+    // let res;
+    // if (onEdit) {
+    //   res = await putData(
+    //     `product/${id}`,
+    //     { ...product, images: [...imgOldURL, ...media] },
+    //     auth.token
+    //   );
+    //   if (res.err)
+    //     return dispatch({ type: 'NOTIFY', payload: { error: res.err } });
+    // } else {
+    //   res = await postData(
+    //     'product',
+    //     { ...product, images: [...imgOldURL, ...media] },
+    //     auth.token
+    //   );
+    //   if (res.err)
+    //     return dispatch({ type: 'NOTIFY', payload: { error: res.err } });
+    // }
 
-    // const res = await postData(
-    //   'product',
-    //   { ...product, images: [...imgOldURL, ...media] },
-    //   auth.token
-    // );
-    // if (res.err)
-    //   return dispatch({ type: 'NOTIFY', payload: { error: res.err } });
+    const res = await postData(
+      "product",
+      { ...product, images: [...imgOldURL, ...media] },
+      auth.token
+    );
+    if (res.err)
+      return dispatch({ type: "NOTIFY", payload: { error: res.err } });
 
-    return dispatch({ type: 'NOTIFY', payload: { success: res.msg } });
+    return dispatch({ type: "NOTIFY", payload: { success: res.msg } });
   };
 
   return (
@@ -229,7 +229,7 @@ const ProductsManager = () => {
           </div>
 
           <button type="submit" className="btn btn-info my-2 px-4">
-            {onEdit ? 'Update' : 'Create'}
+            {onEdit ? "Update" : "Create"}
           </button>
         </div>
 
